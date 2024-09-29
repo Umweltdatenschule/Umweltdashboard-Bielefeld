bab21b68c2891fdcdf81d5ef7c642366a18bf87f
illia <ichorny.ilya25@gmail.com>
2024-09-29 17:07:41 +0200

    add iframe

--- src/app/(dashboard)/city/[id]/page.tsx
+++ src/app/(dashboard)/city/[id]/page.tsx
@@ -1,5 +1,6 @@
 import Airquality from "@/components/airQuality/AirQualitySection/Airquality";
 import GreenvalueSection from "@/components/greenValues/GreenValuesSection/GreenvalueSection";
+import Iframes from "@/components/Iframes/Iframes";
 import PageInfo from "@/components/pageinfo/PageInfo";
 import React from "react";
 
@@ -9,6 +10,7 @@ const Page = () => {
       <PageInfo />
       <Airquality />
       <GreenvalueSection />
+      <Iframes />
     </div>
   );
 };
